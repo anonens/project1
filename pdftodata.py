0887ea4ceb3cef8d7bc7b53ebfb0210fccd51cb3
Жаннур <zhannur@MacBook-Zhannur.local>
2025-07-17 17:43:24 +0500

    Первый коммит

---
+++ pdftodata.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "775a7e44-b77f-4b26-8cb6-35ce440b55f3",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
